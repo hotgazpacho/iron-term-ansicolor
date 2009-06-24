bfd5037effdc9cea813173b80124baed164f2d64
hotgazpacho <will@hotgazpacho.org>
2009-06-24 01:22:36 -0400

    Added an example for Black text, and cleaned up the existing examples

--- lib/iron-term-ansicolor.rb
+++ lib/iron-term-ansicolor.rb
@@ -9,16 +9,19 @@ end
 
 module Kernel
   alias_method :original_puts, :puts
+  
   def puts(*args)
     args.each do |arg|
       originalColor = System::Console.ForegroundColor
       matches = ANSI_REGEXP.match(arg)
       color_code = matches[1]
       case color_code.to_i
-        when 31 
-          System::Console.ForegroundColor = System::ConsoleColor.Red
-        when 32
-          System::Console.ForegroundColor = System::ConsoleColor.Green
+      when 30
+        System::Console.ForegroundColor = System::ConsoleColor.Black
+      when 31 
+        System::Console.ForegroundColor = System::ConsoleColor.Red
+      when 32
+        System::Console.ForegroundColor = System::ConsoleColor.Green
       end
       original_puts(matches[2])
       System::Console.ForegroundColor = originalColor
--- spec/iron-term-ansicolor_spec.rb
+++ spec/iron-term-ansicolor_spec.rb
@@ -1,21 +1,32 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "IronTermANSIColor" do
-  it "should set the foreground color to red when presented with a string with only red ANSI control code" do
-    string = "Red".red
-    original_color = System::ConsoleColor.Gray
-    System::Console.should_receive(:ForegroundColor).once.and_return(original_color)
-    System::Console.should_receive(:ForegroundColor=).ordered.with(System::ConsoleColor.Red)
-    System::Console.should_receive(:ForegroundColor=).ordered.with(original_color)
+  
+  before(:all) do
+    @original_color = System::ConsoleColor.Gray
+  end
+  
+  it "should set the foreground color to black when doing a puts of a string with only black ANSI control code and reset the foreground color after" do
+    string = "Black".black
+    System::Console.should_receive(:ForegroundColor).once.and_return(@original_color)
+    System::Console.should_receive(:ForegroundColor=).ordered.with(System::ConsoleColor.Black)
+    System::Console.should_receive(:ForegroundColor=).ordered.with(@original_color)
     puts string
   end
   
-  it "should set the foreground color to green when presented with a string with only green ANSI control code" do
+  it "should set the foreground color to red when doing a puts of a string with only red ANSI control code and reset the foreground color after" do
+    string = "Red".red
+    System::Console.should_receive(:ForegroundColor).once.and_return(@original_color)
+    System::Console.should_receive(:ForegroundColor=).ordered.with(System::ConsoleColor.Red)
+    System::Console.should_receive(:ForegroundColor=).ordered.with(@original_color)
+    puts string
+  end
+  
+  it "should set the foreground color to green when doing a puts of a string with only green ANSI control code and reset the foreground color after" do
     string = "Green".green
-    original_color = System::ConsoleColor.Gray
-    System::Console.should_receive(:ForegroundColor).once.and_return(original_color)
+    System::Console.should_receive(:ForegroundColor).once.and_return(@original_color)
     System::Console.should_receive(:ForegroundColor=).ordered.with(System::ConsoleColor.Green)
-    System::Console.should_receive(:ForegroundColor=).ordered.with(original_color)
+    System::Console.should_receive(:ForegroundColor=).ordered.with(@original_color)
     puts string
   end
 end
