5568371f226e0bbc3f7edea0c720de4a09d8d4cb
Danny Coates <dannycoates@gmail.com>
2010-02-26 15:27:19 +0800

    modified code to better work with Cucumber

--- lib/iron-term-ansicolor.rb
+++ lib/iron-term-ansicolor.rb
@@ -1,73 +1,97 @@
 require 'mscorlib'
 require 'term/ansicolor'
 
-## add the trailing m  because some of the rspec output spans multiple lines
-ANSI_REGEXP = /\e\[(.+?)m(.+?)(?=(\e\[0m|\z))/m
-SsC=System::ConsoleColor
-SC = System::Console
-
 class String
   include Term::ANSIColor
 end
 
 module IronTermAnsiColor
+  include System
   module Version
     MAJOR=0
     MINOR=0
-    PATCH=1
+    PATCH=2
     STRING="#{MAJOR}.#{MINOR}.#{PATCH}"
+  end
+  
+  ANSI_REGEXP = /\e\[([1349][0-7][0-7]?|[01])m/
+  
+  FGCOLORS = {
+    30 => ConsoleColor.black,
+    31 => ConsoleColor.red,
+    32 => ConsoleColor.dark_green,
+    33 => ConsoleColor.dark_yellow,
+    34 => ConsoleColor.dark_blue,
+    35 => ConsoleColor.dark_magenta,
+    36 => ConsoleColor.dark_cyan,
+    37 => ConsoleColor.gray,
+    90 => ConsoleColor.gray,
+    91 => ConsoleColor.red,
+    92 => ConsoleColor.green,
+    93 => ConsoleColor.yellow,
+    94 => ConsoleColor.blue,
+    95 => ConsoleColor.magenta,
+    96 => ConsoleColor.cyan,
+    97 => ConsoleColor.white
+  }
+  
+  BGCOLORS = {
+    40 => ConsoleColor.black,
+    41 => ConsoleColor.dark_red,
+    42 => ConsoleColor.dark_green,
+    43 => ConsoleColor.dark_yellow,
+    44 => ConsoleColor.dark_blue,
+    45 => ConsoleColor.dark_magenta,
+    46 => ConsoleColor.dark_cyan,
+    47 => ConsoleColor.gray,
+    100 => ConsoleColor.gray,
+    101 => ConsoleColor.red,
+    102 => ConsoleColor.green,
+    103 => ConsoleColor.yellow,
+    104 => ConsoleColor.blue,
+    105 => ConsoleColor.magenta,
+    106 => ConsoleColor.cyan,
+    107 => ConsoleColor.white
+  }  
+    
+  def set_color(num)
+    if num == 0
+      Console.reset_color
+    elsif num == 1
+      #since we can't do bold, invert the colors
+      bg = Console.background_color
+      Console.background_color = Console.foreground_color
+      Console.foreground_color = bg       
+    else
+      Console.foreground_color = FGCOLORS[num] || Console.foreground_color
+      Console.background_color = BGCOLORS[num] || Console.background_color
+    end
   end
 end
 
 class IO
-  alias_method :original_write, :write
-  alias_method :original_puts, :puts
+  include IronTermAnsiColor
   
   def puts(*args)
-    rewrite(*args) { |text| original_puts text }
+    args.each {|str| write(str)}
+    syswrite "\n"
     nil
   end
-
-  def write(*args)
-    rewrite(*args) { |text| original_write text }
-  end
-
-  private
-    def extract_ansi_data(clr, txt)
-      fg_color_map =    Hash[30,SsC.black,31,SsC.red,32,SsC.dark_green,33,SsC.dark_yellow,34,SsC.dark_blue,35,SsC.dark_magenta,
-        36,SsC.dark_cyan,37,SsC.gray]
-      bg_color_map = Hash[40,SsC.black,41,SsC.dark_red,42,SsC.dark_green,43,SsC.dark_yellow,44,SsC.dark_blue,45,SsC.dark_magenta,
-        46,SsC.dark_cyan,47,SsC.gray]
-    
-      #matches = ANSI_REGEXP.match(arg)
-    
-      fg_color = fg_color_map[clr] || SC.foreground_color
-      bg_color = bg_color_map[clr] || SC.background_color
-    
-      { :foreground => fg_color, :background => bg_color,:text => txt}
-    end
   
-    def rewrite(*args, &b)
-      raise ArgumentError, "needs a block" unless b
-      result=0
-      args.each do |arg|
-        fg_color = SC.foreground_color
-        bg_color = SC.background_color
-
-        ##dkb
-        data =  if ANSI_REGEXP =~ arg
-                  extract_ansi_data($1.to_i, $2)
-                else
-                  {:text => arg}
-                end  
-
-        SC.foreground_color = data[:foreground] || fg_color
-        SC.background_color = data[:background] || bg_color
-        result = b.call data[:text]
-        SC.background_color = bg_color
-        SC.foreground_color = fg_color
-      end
-      result
+  def write(str)
+    result = 0
+    str = str.to_s
+    match = ANSI_REGEXP.match(str)
+    if match.nil?
+      #no ansi code in string
+      result += syswrite str
+    else
+      #write what comes before the ansi code in the current color
+      result += syswrite str[0, match.begin(1) - 2]
+      set_color(match[1].to_i)
+      #write what comes after the ansi code in the new color
+      result += write(str[(match.end(1) + 1)..-1])
     end
-
+    result
+  end
 end
