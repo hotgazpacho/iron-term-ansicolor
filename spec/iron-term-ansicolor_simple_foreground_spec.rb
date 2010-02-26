d5056347c2f4c45b46d2d37bb8e9aedc43ee4d05
Will Green <will@hotgazpacho.org>
2010-02-26 15:48:11 -0500

    Changed the specs to make them pass

--- spec/iron-term-ansicolor_simple_foreground_spec.rb
+++ spec/iron-term-ansicolor_simple_foreground_spec.rb
@@ -8,65 +8,57 @@ describe "IronTermANSIColor", "when given a string with ANSI foreground color co
   
   it "should set the foreground color to black when doing a puts of a string with only blackforeground ANSI control code and reset the foreground color after" do
     string = "Black".black
-    System::Console.should_receive(:foreground_color).twice.and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).once.with(System::ConsoleColor.black)
-    System::Console.should_receive(:foreground_color=).at_least(3).times.with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
   
   it "should set the foreground color to dark red when doing a puts of a string with only redforeground ANSI control code and reset the foreground color after" do
     string = "Red".red
-    System::Console.should_receive(:foreground_color).at_least(:once).and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).once.with(System::ConsoleColor.red)
-    System::Console.should_receive(:foreground_color=).at_least(:once).with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
   
   it "should set the foreground color to dark green when doing a puts of a string with only greenforeground ANSI control code and reset the foreground color after" do
     string = "Green".green
-    System::Console.should_receive(:foreground_color).at_least(:once).and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).once.with(System::ConsoleColor.dark_green)
-    System::Console.should_receive(:foreground_color=).at_least(:once).with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
   
   it "should set the foreground color to dark yellow when doing a puts of a string with only yellowforeground ANSI control code and reset the foreground color after" do
     string = "Yellow".yellow
-    System::Console.should_receive(:foreground_color).at_least(:once).and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).once.with(System::ConsoleColor.DarkYellow)
-    System::Console.should_receive(:foreground_color=).at_least(:once).with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
   
   it "should set the foreground color to dark blue when doing a puts of a string with only blueforeground ANSI control code and reset the foreground color after" do
     string = "Blue".blue
-    System::Console.should_receive(:foreground_color).at_least(:once).and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).once.with(System::ConsoleColor.DarkBlue)
-    System::Console.should_receive(:foreground_color=).at_least(:once).with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
   
   it "should set the foreground color to dark magenta when doing a puts of a string with only magentaforeground ANSI control code and reset the foreground color after" do
     string = "Magenta".magenta
-    System::Console.should_receive(:foreground_color).at_least(:once).and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).at_least(:once).with(System::ConsoleColor.DarkMagenta)
-    System::Console.should_receive(:foreground_color=).at_least(:once).with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
   
   it "should set the foreground color to dark cyan when doing a puts of a string with only cyanforeground ANSI control code and reset the foreground color after" do
     string = "Cyan".cyan
-    System::Console.should_receive(:foreground_color).at_least(:once).and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).once.with(System::ConsoleColor.DarkCyan)
-    System::Console.should_receive(:foreground_color=).at_least(:once).with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
   
   it "should set the foreground color to gray when doing a puts of a string with only whiteforeground ANSI control code and reset the foreground color after" do
     string = "Gray".white
-    System::Console.should_receive(:foreground_color).at_least(:once).and_return(@original_fgcolor)
     System::Console.should_receive(:foreground_color=).at_least(:once).with(System::ConsoleColor.Gray)
-    System::Console.should_receive(:foreground_color=).at_least(:once).with(@original_fgcolor)
+    System::Console.should_receive(:reset_color).at_least(:once)
     puts string
   end
 end
